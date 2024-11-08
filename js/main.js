f0082d266cc16c0dea0e042eb4045715b46653de
qerrlo <beatraceroff@gmail.com>
2024-11-08 12:18:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,16 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const username = "recent";
-//   const projectsContainer = document.getElementById("projects");
+document.addEventListener("DOMContentLoaded", () => {
+  const username = "recent";
+  const projectsContainer = document.getElementById("projects");
 
-//   fetch(`https://red.mat6tube.com/${username}`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       const project = document.createElement("div");
-//       project.classList.add("project");
+  const project = document.createElement("div");
+  project.classList.add("project");
 
-//       project.innerHTML = `
-//         <img src="assets/logo.png" alt="Project Logo">
-//         <h3>${data.name || "Название проекта"}</h3>
-//         <p>${data.description || "Нет описания"}</p>
-//         <a href="https://red.mat6tube.com/recent" target="_blank">Перейти на red.mat6tube.com/recent</a>
-//       `;
-
-//       projectsContainer.appendChild(project);
-//     })
-//     .catch((error) =>
-//       console.error("Error fetching repository data:", error)
-//     );
-// });
+  project.innerHTML = `
+    <img src="img/logo.png" alt="ya eby?">
+                    <h3>Mat6tube</h3>
+                    <p>"No Description"}</p>
+                    <a href="red.mat6tube.com/recent
+                    }" target="_blank">View on GitHub</a>
+                `;
+  projectsContainer.appendChild(project);
+});

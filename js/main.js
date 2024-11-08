4e87089b9dfbf7f1c52d9558232b2706e581f7d6
qerrlo <beatraceroff@gmail.com>
2024-11-08 22:16:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   project.innerHTML = `
     <img src="assets/logo.png" alt="red">
-                    <h3>Red Mat6 Tube</h3>
+                    <h3>Mat6Tube</h3>
                     <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
                     <a href="https://red.mat6tube.com/recent" target="_blank">Перейти на сайт</a>`;
+  projectsContainer.appendChild(project);
+
+    project.innerHTML = `
+    <img src="assets/logo.png" alt="red">
+                    <h3>Noodlemagazine</h3>
+                    <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
+                    <a href="https://adult.noodlemagazine.com/new-video" target="_blank">Перейти на сайт</a>`;
   projectsContainer.appendChild(project);
 });

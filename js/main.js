351654aab7ea0932b7d00e7dcab482bf647ce5bc
qerrlo <beatraceroff@gmail.com>
2024-11-08 12:11:24 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const username = [
-    "recent",
-  ];
+  const username = "recent";
   const projectsContainer = document.getElementById("projects");
 
-  username.forEach((repo) => {
-    fetch(`https://red.mat6tube.com/${username}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const project = document.createElement("div");
-        project.classList.add("project");
+  fetch(`https://red.mat6tube.com/${username}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const project = document.createElement("div");
+      project.classList.add("project");
 
-        project.innerHTML = `
-                    <img src="assets/logo.png" alt="${data.name}">
-                    <h3>${data.name}</h3>
-                    <p>${data.description || "Нет описание"}</p>
-                    // <a href="${data.html_url}" target="_blank">Перейти на сайт</a>
-                    <a href="https://red.mat6tube.com/recent" target="_blank">Перейти на red.mat6tube.com/recent</a>
-                `;
+      project.innerHTML = `
+        <img src="assets/logo.png" alt="Project Logo">
+        <h3>${data.name || "Название проекта"}</h3>
+        <p>${data.description || "Нет описания"}</p>
+        <a href="https://red.mat6tube.com/recent" target="_blank">Перейти на red.mat6tube.com/recent</a>
+      `;
 
-        projectsContainer.appendChild(project);
-      })
-      .catch((error) =>
-        console.error("Error fetching repository data:", error)
-      );
-  });
+      projectsContainer.appendChild(project);
+    })
+    .catch((error) =>
+      console.error("Error fetching repository data:", error)
+    );
 });

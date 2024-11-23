4a914689b41c4d2974a686e5b9e82e028e171955
qerrlo <beatraceroff@gmail.com>
2024-11-23 15:03:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   project.classList.add("project");
   const project2 = document.createElement("div");
   project2.classList.add("project");
+  const project3 = document.createElement("div");
+  project3.classList.add("project");
 
   project.innerHTML = `
     <img src="assets/logo.png" alt="red">
@@ -14,10 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     <a href="https://wow.mat6tube.com/recent" target="_blank">Перейти на сайт</a>`;
   projectsContainer.appendChild(project);
 
-    project2.innerHTML = `
+  project2.innerHTML = `
     <img src="assets/logo.png" alt="red">
                     <h3>Noodlemagazine</h3>
                     <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
                     <a href="https://adult.noodlemagazine.com/new-video" target="_blank">Перейти на сайт</a>`;
   projectsContainer.appendChild(project2);
+
+  project3.innerHTML = `
+    <img src="assets/logo.png" alt="red">
+                    <h3>Sex-Studentki</h3>
+                    <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
+                    <a href="https://sex-studentki.love/" target="_blank">Перейти на сайт</a>`;
+  projectsContainer.appendChild(project3);
+  
 });

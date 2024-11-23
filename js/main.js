925fe2f40eb25e7357f1ea23b135b8e2f2bccbc3
qerrlo <beatraceroff@gmail.com>
2024-11-23 16:20:16 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,11 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     <a href="https://adult.noodlemagazine.com/new-video" target="_blank">Перейти на сайт</a>`;
   projectsContainer.appendChild(project2);
 
-  project3.innerHTML = `
-    <img src="assets/logo.png" alt="red">
-                    <h3>Sex-Studentki</h3>
-                    <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
-                    <a href="https://sex-studentki.love/" target="_blank">Перейти на сайт</a>`;
-  projectsContainer.appendChild(project3);
+  // project3.innerHTML = `
+  //   <img src="assets/logo.png" alt="red">
+  //                   <h3>Sex-Studentki</h3>
+  //                   <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
+  //                   <a href="https://sex-studentki.love/" target="_blank">Перейти на сайт</a>`;
+  // projectsContainer.appendChild(project3);
   
 });

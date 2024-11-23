5991678dda1443f03a43c807cd7beede1fff386e
qerrlo <beatraceroff@gmail.com>
2024-11-23 14:56:58 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <img src="assets/logo.png" alt="red">
                     <h3>Mat6Tube</h3>
                     <p>Данный сайт предназначен для лиц, достигших 18 лет.</p>
-                    <a href="https://red.mat6tube.com/recent" target="_blank">Перейти на сайт</a>`;
+                    <a href="https://wow.mat6tube.com/recent" target="_blank">Перейти на сайт</a>`;
   projectsContainer.appendChild(project);
 
     project2.innerHTML = `
